5337db708a79df54ccedd1c590d8cd0e138b51d3
sayush1204 <94184411+sayush1204@users.noreply.github.com>
2023-05-11 12:52:21 -0400

    ExpenseTrackerGUI.py

--- ExpenseTrackerGUI.py
+++ ExpenseTrackerGUI.py
@@ -9,85 +9,85 @@ from tkinter import PhotoImage
 from maxheap import MaxHeap
 
 
-
-
 class SuiteShareGUI:
     def __init__(self, root_window):
         self.root_window = root_window
         root_window.title("SuiteShare")
+        root_window.geometry("545x520")
         root_window.configure(bg="#4C7330")
         self.load()
 
-        self.image = PhotoImage(file="suiteshare.png")
-
         # Image Label
+        self.image = PhotoImage(file="suiteshare.png")
         self.image = self.image.subsample(2, 2)
         self.label_image = tk.Label(root_window, image=self.image, bg="#4C7330")
-        self.label_image.place(x=62.5)
+        self.label_image.place(x=67)
 
-        # Add User Button
-        self.button_add_user = tk.Button(root_window, text="Add User", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.add_user, pady = 2,)
-        self.button_add_user.grid(row=2, column=0, padx=10, pady=(125, 30), sticky="we")
+        # User Entry Box
+        self.user_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330", width = 15)
+        self.user_entry.grid(row=2, column=0, padx=10, pady=(125, 30), sticky="we")
 
-        self.button_remove_user = tk.Button(root_window, text="Remove User", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.remove_user, pady = 2,)
-        self.button_remove_user.grid(row=2, column=1, padx=10, pady=(125, 30), sticky="we")
+        # Add and Remove User Button
+        self.button_add_user = tk.Button(root_window, text="Add User", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.add_user, pady = 2)
+        self.button_add_user.grid(row=2, column=1, padx=10, pady=(125, 30), sticky="we")
+
+        self.button_remove_user = tk.Button(root_window, text="Remove User", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.remove_user, pady = 2, width=13)
+        self.button_remove_user.grid(padx=(10,90), pady=(125, 30), sticky="we")
+        self.button_remove_user.place(x=374, y=125, width=160)
 
         # Debtor Entry Widget
         self.debtor_label = tk.Label(root_window, text="Debtor:", font=("Segoe UI", 16, "bold"), bg="#4C7330", fg="#D9B777")
-        self.debtor_label.grid(row=3, column=0, padx=10, pady=10, sticky="we")
-        self.debtor_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330")
+        self.debtor_label.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+        self.debtor_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330", width=15)
         self.debtor_entry.grid(row=3, column=1, padx=10, pady=10, sticky="we")
 
         # Creditor Entry Widget
         self.creditor_label = tk.Label(root_window, text="Creditor:", font=("Segoe UI", 16, "bold"), bg="#4C7330", fg="#D9B777")
-        self.creditor_label.grid(row=4, column=0, padx=10, pady=10, sticky="we")
-        self.creditor_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330")
+        self.creditor_label.grid(row=4, column=0, padx=10, pady=10, sticky="e")
+        self.creditor_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330", width=15)
         self.creditor_entry.grid(row=4, column=1, padx=10, pady=10, sticky="we")
 
         # Amount Entry Widget
         self.amount_label = tk.Label(root_window, text="Amount:", font=("Segoe UI", 16, "bold"), bg="#4C7330", fg="#D9B777")
-        self.amount_label.grid(row=5, column=0, padx=10, pady=10, sticky="we")
-        self.amount_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330")
+        self.amount_label.grid(row=5, column=0, padx=10, pady=10, sticky="e")
+        self.amount_entry = tk.Entry(root_window, font=("Segoe UI", 16), bg="#D9B777", fg="#4C7330", width=15)
         self.amount_entry.grid(row=5, column=1, padx=10, pady=10, sticky="we")
 
         # Add Debt and Remove Debt Buttons
-        self.add_debt_button = tk.Button(root_window, text="Add Debt", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.add_debt, pady = 2)
-        self.add_debt_button.grid(row=3, column=2, rowspan = 2, padx=(10,20), pady=(10, 10), sticky="we")
-        self.remove_debt_button = tk.Button(root_window, text="Remove Debt", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.remove_debt, pady = 2)
-        self.remove_debt_button.grid(row=4, column=2, rowspan = 3, padx=(10,20), pady=(10, 10), sticky="we")
+        self.add_debt_button = tk.Button(root_window, text="Add Debt", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.add_debt, pady = 2, width=13)
+        self.add_debt_button.grid(padx=(10,90), pady=(10, 10), sticky="we")
+        self.add_debt_button.place(x=374, y=220, width=160)
 
-        # Settle Debt and Calculate Sales Tax Buttons
+        self.remove_debt_button = tk.Button(root_window, text="Remove Debt", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.remove_debt, pady = 2, width=13)
+        self.remove_debt_button.grid(padx=(10,90), pady=(10, 10), sticky="we")
+        self.remove_debt_button.place(x=374, y=272, width=160)
 
+        # Settle Debt and Calculate Split Tax Buttons
         self.button_clear_debts = tk.Button(root_window, text="Settle Debt", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.clear_debt, pady = 2)
-        self.button_clear_debts.grid(row=7, column=0, padx=10, pady=(40,10), sticky="we")
+        self.button_clear_debts.grid(padx=10, pady=(40,10), sticky="we")
+        self.button_clear_debts.place(x=100, y=370, width=160)
 
-        self.button_sales_tax = tk.Button(root_window, text="Sales Tax", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.sales_tax, pady = 2,)
-        self.button_sales_tax.grid(row=7, column=1, padx=10, pady=(40,10), sticky="we")
-
-        # Save and Load Buttons
-
-        #self.button_save = tk.Button(root_window, text="Save", font=("Courier New", 18, "bold"), bg="#f0f0f0", fg="#333333", command=self.save, pady = 4)
-        #self.button_save.grid(row=1, column=1, padx=10, pady=(30, 30), sticky="we")
-
-        #self.button_load = tk.Button(root_window, text="Load", font=("Courier New", 16, "bold"), bg="#f0f0f0", fg="#333333", command=self.load, pady = 2)
-        #self.button_load.grid(row=8, column=0, padx=10, pady=10, sticky="we")
+        self.button_split_tax = tk.Button(root_window, text="Split Tax", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.split_tax, pady = 2, width=13)
+        self.button_split_tax.grid(padx=10, pady=(40,10), sticky="we",columnspan=1)
+        self.button_split_tax.place(x=285, y=370, width=160)
 
         # Debt Log and Debt Table Buttons
-
         self.button_show_debts = tk.Button(root_window, text="Debt Log", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.show_debts, pady = 2)
-        self.button_show_debts.grid(row=8, column=0, padx=10, pady=10, sticky="we")
+        self.button_show_debts.grid(padx=10, pady=10, sticky="we")
+        self.button_show_debts.place(x=12, y=420, width=160)
 
         self.button_show_graph = tk.Button(root_window, text="Debt Table", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.show_graph, pady = 2)
-        self.button_show_graph.grid(row=8, column=1, padx=10, pady=10, sticky="we")
+        self.button_show_graph.grid(padx=10, pady=10, sticky="we")
+        self.button_show_graph.place(x=193, y=420, width=160)
 
         # Clear Data Button
-
         self.button_clear_data = tk.Button(root_window, text="Clear Data", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.clear_data, pady = 2)
-        self.button_clear_data.grid(row=9, column=0, padx=10, pady=(10,20), sticky="we")
+        self.button_clear_data.grid(row=10, column=1, padx=10, pady=(135,30), sticky="we")
 
-        self.button_sort_users = tk.Button(root_window, text="Sort Users", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.sort_users_by_debt, pady=2)
-        self.button_sort_users.grid(row=7, column=2, padx=(10, 20), pady=(40, 10), sticky="we")
-
+        # Sort Users Button
+        self.button_sort_users = tk.Button(root_window, text="Sort Users", font=("Segoe UI", 16, "bold"), bg="#D9B777", fg="#4C7330", command=self.sort_users_by_debt, pady=2, width=13)
+        self.button_sort_users.grid(padx=(10, 90), pady=(10, 10), sticky="we")
+        self.button_sort_users.place(x=374, y=420, width=160)
 
         # Create data structures
         self.users = []
@@ -98,12 +98,14 @@ class SuiteShareGUI:
         self.graph = nx.DiGraph()
 
 
+
+# SuiteShare Methods
+
     def load(self):
         self.total_debt = {}
         # Load users and debts data from CSV files
         self.users_df = pd.read_csv("users.csv")
         self.users = list(self.users_df["Name"])
-
         self.debts = pd.read_csv("debts.csv")
 
         # Load graph data from CSV file
@@ -118,10 +120,34 @@ class SuiteShareGUI:
                 self.total_debt[user] = float(debt)
 
 
+
+    def save(self):
+        # Save user names to users.csv
+        pd.DataFrame(self.users, columns=["Name"]).to_csv("users.csv", index=False)
+
+        # Save debts to debts.csv
+        self.debts.to_csv("debts.csv", index=False)
+
+        # Save graph to graph.csv
+        nodes = sorted(list(self.graph.nodes()))
+        adjacency_matrix = pd.DataFrame(0, index=nodes, columns=nodes)
+        for edge in self.graph.edges():
+            from_user, to_user = edge
+            amount = self.graph.get_edge_data(*edge)["weight"]
+            adjacency_matrix.at[from_user, to_user] = amount
+        adjacency_matrix.to_csv("graph.csv")
+
+        # Save user debts to a file
+        with open("user_debts.txt", "w") as file:
+            for user, debt in self.total_debt.items():
+                file.write(f"{user},{debt}\n")
+
+
+
     def add_user(self):
         self.load()
         # Get user name from user input
-        user_name = tk.simpledialog.askstring("Add User", "Enter user name:")
+        user_name = self.user_entry.get()
 
         # Check if the user name already exists
         if user_name in self.users:
@@ -137,26 +163,30 @@ class SuiteShareGUI:
         # Display a message confirming that the user has been added
         tk.messagebox.showinfo("Success", f"User {user_name} has been added.")
         self.save()
+        self.user_entry.delete(0, tk.END)
+
+
 
     def remove_user(self):
         self.load()
         # Get user name from user input
-        user_name = tk.simpledialog.askstring("Remove User", "Enter user name:")
+        user_name = self.user_entry.get()
 
         # Check if the user name doesn't exists
-        if user_name  not in self.users:
+        if user_name not in self.users:
             tk.messagebox.showerror("Error", "User does not exist.")
             return
 
-        # Add the user to the users list
+        # Remove the user from the users list
         self.users.remove(user_name)
 
-        # Add the user as a node to the graph
+        # Remove the user as a node from the graph
         self.graph.remove_node(user_name)
 
         # Display a message confirming that the user has been added
         tk.messagebox.showinfo("Success", f"User {user_name} has been removed.")
         self.save()
+        self.user_entry.delete(0, tk.END)
 
 
 
@@ -175,6 +205,14 @@ class SuiteShareGUI:
             "Amount": amount
         }, ignore_index=True)
 
+        # Check if the user name doesn't exists
+        if from_user not in self.users:
+            tk.messagebox.showerror("Error", "One or both users does not not exist.")
+            return
+        elif to_user not in self.users:
+            tk.messagebox.showerror("Error", "One or both users does not not exist.")
+            return
+        
         # Update graph with new debt
         if from_user not in self.users:
             self.users.append(from_user)
@@ -202,6 +240,8 @@ class SuiteShareGUI:
         self.creditor_entry.delete(0, tk.END)
         self.amount_entry.delete(0, tk.END)
 
+
+
     def remove_debt(self):
         self.load()
          # Get debt details from user input
@@ -217,6 +257,14 @@ class SuiteShareGUI:
             "Amount": -amount
         }, ignore_index=True)
 
+        # Check if the user name doesn't exists
+        if from_user not in self.users:
+            tk.messagebox.showerror("Error", "One or both users does not not exist.")
+            return
+        elif to_user not in self.users:
+            tk.messagebox.showerror("Error", "One or both users does not not exist.")
+            return
+        
         # Update graph with new debt
         if from_user not in self.users:
             self.users.append(from_user)
@@ -245,11 +293,17 @@ class SuiteShareGUI:
         self.amount_entry.delete(0, tk.END)
 
 
+
     def clear_debt(self):
         self.load()
         # Get user name from user input
         user = tk.simpledialog.askstring("Clear Debt", "Enter the name of the user to clear debts for:")
 
+        # Check if the user name doesn't exists
+        if user not in self.users:
+            tk.messagebox.showerror("Error", "User does not exist.")
+            return
+        
         # Remove all debts involving the user
         self.debts = self.debts.loc[(self.debts["From"] != user) & (self.debts["To"] != user)]
         self.total_debt = {}
@@ -264,56 +318,21 @@ class SuiteShareGUI:
         self.save()
 
 
-    def sales_tax(self):
-        # Code to calculate sales tax for debts
-        pass
 
+    def split_tax(self):
+        self.load()
+        total_cost = tk.simpledialog.askfloat("Total Cost", "Enter the total cost of the order.")
+        total_tax = tk.simpledialog.askfloat("Total Tax", "Enter the total taxes/fees of the order.")
+        partial_cost = tk.simpledialog.askfloat("Partial Cost", "Enter the partial cost of the order you want to calculate fees for.")
 
+        if partial_cost > total_cost:
+            tk.messagebox.showerror("Error", "Partial cost is greater than the total cost of the order.")
+            return
+        
+        weighted_tax = (partial_cost/total_cost)*total_tax
+        weighted_cost = partial_cost+weighted_tax
 
-    def save(self):
-        # Save user names to users.csv
-        pd.DataFrame(self.users, columns=["Name"]).to_csv("users.csv", index=False)
-
-        # Save debts to debts.csv
-        self.debts.to_csv("debts.csv", index=False)
-
-        # Save graph to graph.csv
-        nodes = sorted(list(self.graph.nodes()))
-        adjacency_matrix = pd.DataFrame(0, index=nodes, columns=nodes)
-        for edge in self.graph.edges():
-            from_user, to_user = edge
-            amount = self.graph.get_edge_data(*edge)["weight"]
-            adjacency_matrix.at[from_user, to_user] = amount
-        adjacency_matrix.to_csv("graph.csv")
-
-        # Save user debts to a file
-        with open("user_debts.txt", "w") as file:
-            for user, debt in self.total_debt.items():
-                file.write(f"{user},{debt}\n")
-
-
-
-    def load(self):
-        self.total_debt = {}
-        # Load users and debts data from CSV files
-        self.users_df = pd.read_csv("users.csv")
-        self.users = list(self.users_df["Name"])
-
-        self.debts = pd.read_csv("debts.csv")
-
-        # Load graph data from CSV file
-        graph_df = pd.read_csv("graph.csv", index_col=0, header=0)
-        self.graph = nx.DiGraph(graph_df.values)
-        self.graph = nx.relabel_nodes(self.graph, dict(enumerate(graph_df.columns)))
-
-        # Load user debts from a file
-        with open("user_debts.txt", "r") as file:
-            for line in file:
-                user, debt = line.strip().split(",")
-                self.total_debt[user] = float(debt)
-
-        # Display a message confirming that everything has been loaded in
-        #tk.messagebox.showinfo("Success", f"All debts have been loaded in.")
+        tk.messagebox.showinfo("Success", f"The weighted cost with tax for this user is ${weighted_cost:.2f}")
 
 
 
@@ -402,7 +421,6 @@ class SuiteShareGUI:
 
 
     def sort_users_by_debt(self):
-
         self.load()
         # Create a max heap to store the users based on their total debt
         max_heap = MaxHeap()
@@ -417,30 +435,26 @@ class SuiteShareGUI:
         sorted_users_window = tk.Toplevel(self.root_window)
 
         # Create a treeview widget to display the sorted users
-        tree = ttk.Treeview(sorted_users_window, columns=("User", "Debt"))
+        tree = ttk.Treeview(sorted_users_window, columns=("Debt",))
 
         # Set the headings for the columns
-        tree.heading("User", text="User")
+        tree.heading("#0", text="User")
         tree.heading("Debt", text="Debt")
+
+        # Create a set to keep track of the displayed users
+        displayed_users = set()
 
         # Insert the sorted users into the treeview
         while not max_heap.is_empty():
             debt, user = max_heap.extract_max()
-            tree.insert("", "end", text=user, values=(user, f"${-debt:.2f}"))
+
+            # Check if the user has already been displayed
+            if user not in displayed_users:
+                displayed_users.add(user)
+                tree.insert("", "end", text=user, values=(f"${-debt:.2f}",))
 
         # Grid the treeview into the window
         tree.grid()
 
         # Set the window title
         sorted_users_window.title("Users Sorted by Debt")
-
-
-
-
-
-'''
-Problems:
-
-
-
-'''
